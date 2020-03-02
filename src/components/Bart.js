4defaea247cffd0dab421dcb2b8d073e68e90071
Graham Clark <graham@honeybook.com>
2020-03-02 12:02:51 -0800

    more accurate times

--- src/components/Bart.js
+++ src/components/Bart.js
@@ -10,7 +10,7 @@ const CONSTANTS = {
     APIKEY: "MW9S-E7SL-26DU-VV8V",
     BASEURL: {
       STATION: "https://api.bart.gov/api/etd.aspx?cmd=etd&",
-      ARRIVE: "https://api.bart.gov/api/sched.aspx?cmd=depart&",
+      DEPART: "https://api.bart.gov/api/sched.aspx?cmd=depart&b=0&",
     },
   },
 }
@@ -22,7 +22,7 @@ const Bart = () => {
   const [selectedStartStation, setSelectedStartStation] = useState(4)
   const [selectedDestStation, setSelectedDestStation] = useState(7)
   const [urlQuery, setUrlQuery] = useState(
-    "https://api.bart.gov/api/sched.aspx?cmd=depart&orig=phil&dest=bayf&key=MW9S-E7SL-26DU-VV8V&json=y"
+    "https://api.bart.gov/api/sched.aspx?cmd=depart&b=0&orig=phil&dest=bayf&key=MW9S-E7SL-26DU-VV8V&json=y"
   )
 
   const formatTime = Utils.formatTime
@@ -34,7 +34,7 @@ const Bart = () => {
   ) => {
     stationList.length > 0 &&
       setUrlQuery(
-        `${CONSTANTS.BART.BASEURL.ARRIVE}orig=${stationList[selectedStartStation].abbr}&dest=${stationList[selectedDestStation].abbr}&key=${CONSTANTS.BART.APIKEY}&json=y`
+        `${CONSTANTS.BART.BASEURL.DEPART}orig=${stationList[selectedStartStation].abbr}&dest=${stationList[selectedDestStation].abbr}&key=${CONSTANTS.BART.APIKEY}&json=y`
       )
   }
 
